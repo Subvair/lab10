bb1d056511fab9e8f4a8bd2d8abc74db0cdb2a36
Vadim <vadimogienko42k@gmail.com>
2025-05-10 17:13:18 +0700

    Добавлен проект с unit-тестами и тест конструктора на корректную инициализацию размера матрицы

--- MatrixLib.Tests/Test1.cs
+++ MatrixLib.Tests/Test1.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace MatrixCalculatorTests
+{
+  [TestClass]
+  public class SquareMatrixTests
+  {
+    [TestMethod]
+    public void Constructor_ValidSize_ShouldSetMatrixSize()
+    {
+      const int expectedSize = 3;
+
+      SquareMatrix matrix = CreateMatrixWithData(expectedSize, new double[,] {
+        { 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 }
+      });
+
+      Assert.AreEqual(expectedSize, matrix.Size);
+    }
+    
+  }
+}
