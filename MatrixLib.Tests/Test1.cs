eea5b1a167a2575588eca0531f661b8cc7ba5f56
Vadim <vadimogienko42k@gmail.com>
2025-05-10 17:17:15 +0700

    Добавлен тест на исключение при создании матрицы с некорректным размером и тест на корректное выполнение операции сложения матриц

--- MatrixLib.Tests/Test1.cs
+++ MatrixLib.Tests/Test1.cs
@@ -21,6 +21,41 @@ namespace MatrixCalculatorTests
 
       Assert.AreEqual(expectedSize, matrix.Size);
     }
-    
+
+     [TestMethod]
+    [ExpectedException(typeof(MatrixException))]
+    public void Constructor_NonPositiveSize_ShouldThrowMatrixException()
+    {
+      _ = new SquareMatrix(0);
+    }
+
+    [TestMethod]
+    public void Add_MatricesWithSameSize_ShouldReturnCorrectSum()
+    {
+      double[,] dataA = {
+        { 1, 2 },
+        { 3, 4 }
+      };
+
+      double[,] dataB = {
+        { 5, 6 },
+        { 7, 8 }
+      };
+
+      double[,] expectedData = {
+        { 6, 8 },
+        { 10, 12 }
+      };
+
+      SquareMatrix matrixA = CreateMatrixWithData(2, dataA);
+      SquareMatrix matrixB = CreateMatrixWithData(2, dataB);
+      SquareMatrix expectedMatrix = CreateMatrixWithData(2, expectedData);
+
+      SquareMatrix resultMatrix = matrixA + matrixB;
+
+      Assert.AreEqual(expectedMatrix, resultMatrix);
+    }
+
+
   }
 }
